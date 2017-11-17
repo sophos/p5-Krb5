72441a0d7f0ddce0bdda8d5df9da10d6d81b5446
Ioan Rogers <ioan.rogers@sophos.com>
2017-11-17 10:27:11 -0800

    Re-arranged and removed things to use `Dist::Zilla`
    * Split object POD into indidual files
    * Added more POD formatting to improve the aesthetics and add links
    * Removed various POD pieces which are autogenerated by `Pod::Weaver`
    * Removed/renamed files which are autogenerated by `Dist::Zilla`
    * Use `Module::Build` instead of `MakeMaker`, just because I'm more familiar
      with it.
    * Use `Devel::CheckLib` to check krb5 library and headers are available. There
      are test failures on CPANTesters where compilation has failed because they
      are missing
    * Use `PkgConfig` to determine build flags. Let's see what platforms this breaks.

---
+++ inc/My/Builder.pm
@@ -0,0 +1,36 @@
+package My::Builder;
+
+use 5.004;
+use strict;
+use warnings;
+use base 'Module::Build';
+
+use PkgConfig;
+
+sub new {
+    my ($class, %args) = @_;
+
+    my $pkg_name = 'krb5';
+
+    my $pc = PkgConfig->find($pkg_name);
+    if ($pc->errmsg) {
+        die "Failed to get pkg-config info for krb5!\n";
+    }
+
+    printf "Found %s kerberos 5 version %s\n", $pc->get_var('vendor'),
+      $pc->pkg_version;
+
+    if ($pc->get_cflags) {
+        $args{extra_compiler_flags} = $pc->get_cflags;
+        print "CFLAGS: $args{extra_compiler_flags}\n";
+    }
+
+    $args{extra_linker_flags} = $pc->get_ldflags;
+    print "LDFLAGS: $args{extra_linker_flags}\n";
+
+    my $builder = Module::Build->new(%args);
+
+    return $builder;
+}
+
+1;
