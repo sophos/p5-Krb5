8d9a675ad0ff8382d120ede9fe0cc7561fd9128c
Ioan Rogers <ioan.rogers@sophos.com>
2017-11-20 13:05:25 -0800

    Try `krb5-config` if pkgconf is unavailable

--- inc/My/Builder.pm
+++ inc/My/Builder.pm
@@ -11,25 +11,52 @@ sub new {
     my ($class, %args) = @_;
 
     my $pkg_name = 'krb5';
+    my ($cflags, $ldflags, $vendor, $version) = @_;
 
     my $pc = PkgConfig->find($pkg_name);
-    if ($pc->errmsg) {
-        die "Failed to get pkg-config info for krb5!\n";
+    if (!$pc->errmsg) {
+        $cflags  = $pc->get_cflags;
+        $ldflags = $pc->get_ldflags;
+        $vendor  = $pc->get_var('vendor');
+        $version = $pc->pkg_version;
+    } else {
+
+        # no .pc files found, try for krb5-config
+        require File::Which;
+
+        my $krb5_config = File::Which::which('krb5-config');
+
+        if (!$krb5_config) {
+            die "Failed to get pkgconfig info or find krb5-config script!\n";
+        }
+
+        $cflags = `$krb5_config --cflags`;
+        chomp $cflags;
+
+        $ldflags = `$krb5_config --libs`;
+        chomp $ldflags;
+
+        $vendor = `$krb5_config --vendor`;
+        chomp $vendor;
+        $vendor = 'MIT' if $vendor eq 'Massachusetts Institute of Technology';
+
+        $version = `$krb5_config --version`;
+        chomp $version;
+        $version =~ s/Kerberos 5 release //;
     }
 
-    my $vendor = $pc->get_var('vendor');
-    printf "Found %s kerberos 5 version %s\n", $vendor, $pc->pkg_version;
+    printf "Found %s kerberos 5 version %s\n", $vendor, $version;
 
     if ($vendor ne 'MIT') {
         die 'This module currently only supports MIT kerberos';
     }
 
-    if ($pc->get_cflags) {
-        $args{extra_compiler_flags} = $pc->get_cflags;
+    if ($cflags) {
+        $args{extra_compiler_flags} = $cflags;
         print "CFLAGS: $args{extra_compiler_flags}\n";
     }
 
-    $args{extra_linker_flags} = $pc->get_ldflags;
+    $args{extra_linker_flags} = $ldflags;
     print "LDFLAGS: $args{extra_linker_flags}\n";
 
     my $builder = Module::Build->new(%args);
