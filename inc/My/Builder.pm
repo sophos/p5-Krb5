da35bd1d32d10d7b864aa3125abaa44b450813dd
Ioan Rogers <ioan.rogers@sophos.com>
2017-11-20 12:20:09 -0800

    Only build against MIT kerberos, not compatable with Heimdal

--- inc/My/Builder.pm
+++ inc/My/Builder.pm
@@ -17,8 +17,12 @@ sub new {
         die "Failed to get pkg-config info for krb5!\n";
     }
 
-    printf "Found %s kerberos 5 version %s\n", $pc->get_var('vendor'),
-      $pc->pkg_version;
+    my $vendor = $pc->get_var('vendor');
+    printf "Found %s kerberos 5 version %s\n", $vendor, $pc->pkg_version;
+
+    if ($vendor ne 'MIT') {
+        die 'This module currently only supports MIT kerberos';
+    }
 
     if ($pc->get_cflags) {
         $args{extra_compiler_flags} = $pc->get_cflags;
